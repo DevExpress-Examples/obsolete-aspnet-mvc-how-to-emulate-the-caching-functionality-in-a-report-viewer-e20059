f74d38cbd5f32627c3defc92a35cd2fb8dececbd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/DevExpressMvcApplication1/Reports/XtraReport1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using DevExpressMvcApplication1.Models;
+namespace DevExpressMvcApplication1.Reports {
+    partial class XtraReport1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.xrTable1 = new DevExpress.XtraReports.UI.XRTable();
+            this.xrTableRow1 = new DevExpress.XtraReports.UI.XRTableRow();
+            this.xrTableCell5 = new DevExpress.XtraReports.UI.XRTableCell();
+            this.xrTableCell9 = new DevExpress.XtraReports.UI.XRTableCell();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrTable1});
+            this.Detail.HeightF = 25F;
+            this.Detail.Name = "Detail";
+            this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // TopMargin
+            // 
+            this.TopMargin.Name = "TopMargin";
+            this.TopMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // BottomMargin
+            // 
+            this.BottomMargin.Name = "BottomMargin";
+            this.BottomMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(DevExpressMvcApplication1.Models.Product);
+            // 
+            // xrTable1
+            // 
+            this.xrTable1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
+            this.xrTable1.Name = "xrTable1";
+            this.xrTable1.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
+            this.xrTableRow1});
+            this.xrTable1.SizeF = new System.Drawing.SizeF(650F, 25F);
+            // 
+            // xrTableRow1
+            // 
+            this.xrTableRow1.Cells.AddRange(new DevExpress.XtraReports.UI.XRTableCell[] {
+            this.xrTableCell5,
+            this.xrTableCell9});
+            this.xrTableRow1.Name = "xrTableRow1";
+            this.xrTableRow1.Weight = 11.5D;
+            // 
+            // xrTableCell5
+            // 
+            this.xrTableCell5.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "ProductName")});
+            this.xrTableCell5.Name = "xrTableCell5";
+            this.xrTableCell5.Text = "xrTableCell5";
+            this.xrTableCell5.Weight = 1.3007890076044748D;
+            // 
+            // xrTableCell9
+            // 
+            this.xrTableCell9.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "UnitPrice")});
+            this.xrTableCell9.Name = "xrTableCell9";
+            this.xrTableCell9.Text = "xrTableCell9";
+            this.xrTableCell9.Weight = 0.39151868470321749D;
+            // 
+            // XtraReport1
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.TopMargin,
+            this.BottomMargin});
+            this.DataSource = this.bindingSource1;
+            this.Version = "12.1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.TopMarginBand TopMargin;
+        private DevExpress.XtraReports.UI.BottomMarginBand BottomMargin;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private DevExpress.XtraReports.UI.XRTable xrTable1;
+        private DevExpress.XtraReports.UI.XRTableRow xrTableRow1;
+        private DevExpress.XtraReports.UI.XRTableCell xrTableCell5;
+        private DevExpress.XtraReports.UI.XRTableCell xrTableCell9;
+    }
+}
---
+++ VB/DevExpressMvcApplication1/Controllers/HomeController.vb
@@ -0,0 +1,64 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.IO
+Imports System.Web.Mvc
+Imports DevExpress.XtraPrinting.InternalAccess
+Imports DevExpress.XtraReports.UI
+Imports DevExpressMvcApplication1.Models
+Imports DevExpressMvcApplication1.Reports
+Imports System.Linq
+
+Namespace DevExpressMvcApplication1.Controllers
+	Public Class HomeController
+		Inherits Controller
+		Public Class TestModel
+			Private privateReport As XtraReport
+			Public Property Report() As XtraReport
+				Get
+					Return privateReport
+				End Get
+				Set(ByVal value As XtraReport)
+					privateReport = value
+				End Set
+			End Property
+		End Class
+
+		Public Function Index() As ActionResult
+			Dim model As New TestModel()
+			model.Report = GetReport()
+			ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!"
+			Return View(model)
+		End Function
+
+		Public Function DocumentViewerPartial() As ActionResult
+			Dim model As New TestModel()
+			model.Report = GetReport()
+			Return PartialView("DocumentViewerPartial", model)
+		End Function
+
+		Public Function ExportDocumentViewerPartial() As ActionResult
+			Return DevExpress.Web.Mvc.ReportViewerExtension.ExportTo(GetReport())
+		End Function
+
+		Private ms As MemoryStream
+		Private Function GetReport() As XtraReport
+			Dim report As New XtraReport1()
+			AddHandler report.DataSourceDemanded, Function(s, e) AnonymousMethod1(s, e)
+
+			If Session("ReportCache") Is Nothing Then
+				report.CreateDocument()
+				ms = New MemoryStream()
+				PrintingSystemAccessor.SaveIndependentPages(report.PrintingSystem, ms)
+				Session("ReportCache") = ms
+			Else
+				ms = CType(Session("ReportCache"), MemoryStream)
+				PrintingSystemAccessor.LoadVirtualDocument(report.PrintingSystem, ms)
+			End If
+			Return report
+		End Function
+		
+		Private Function AnonymousMethod1(ByVal s As Object, ByVal e As Object) As Boolean
+			CType(s, XtraReport).DataSource = New NorthwindEntities().Products
+			Return True
+		End Function
+	End Class
+End Namespace
